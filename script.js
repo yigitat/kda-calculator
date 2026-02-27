5db68f21aa2faeba3293783c2152ba4e98e33f6f
alp <2828yigitat28@gmail.com>
2026-02-28 01:28:51 +0300

    a

--- script.js
+++ script.js
@@ -17,6 +17,27 @@ function hesaplaRank(game) {
     let rank = "";
     let img = "";
 
+
+    document.getElementById("result").innerHTML =
+        `KDA: ${kda.toFixed(2)}<br>`;
+}
+
+function hesaplaMOBA() {
+
+    let kills = parseInt(document.getElementById("kills").value) || 0;
+    let deaths = parseInt(document.getElementById("deaths").value) || 1;
+    let assists = parseInt(document.getElementById("assists").value) || 0;
+
+    let kda = (kills + assists) / deaths;
+
+    let rating = "";
+
+    if (kda < 1) rating = "Beginner";
+    else if (kda < 2) rating = "Average";
+    else if (kda < 3) rating = "Good";
+    else if (kda < 4) rating = "Pro";
+    else rating = "Godlike";
+
     document.getElementById("result").innerHTML =
         `KDA: ${kda.toFixed(2)}<br>`;
 }
